03364d542e6f2d6ae129bb84613877ad44b09b6d
ksh1024 <ksh251024@gmail.com>
2024-06-13 15:09:57 +0900

    views CategoryListView; templates category_list.html; urls accountbook:category_list

--- householdAccountBook/urls.py
+++ householdAccountBook/urls.py
@@ -21,4 +21,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chart_test/', include('chart_test.urls')),
+    path('accountbook/', include('accountbook.urls')),
 ]
